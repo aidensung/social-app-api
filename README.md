9f54f832cbe70c3665f5cbda667a06030825d8af
Ilhyo Sung <sih8189@gmail.com>
2020-09-16 03:22:40 -0400

    added isFollowing, isMyself api

--- README.md
+++ README.md
@@ -7,8 +7,8 @@ Social networking app with GraphQL, Prisma, React, and React Native
 - [x] Create account
 - [x] Request Secret
 - [x] Confirm Secret (Login)
-- [x] Like / Unlike a photo
-- [x] Comment on a photo
+- [x] Like / Unlike a post
+- [x] Comment on a post
 - [x] Search for users
 - [x] Search for posts by location, caption
 - [x] Follow User
@@ -17,6 +17,6 @@ Social networking app with GraphQL, Prisma, React, and React Native
 - [x] View user profile
 - [x] View my profile
 - [x] View the full post
-- [ ] Upload a photo
-- [ ] Edit the photo (Delete)
+- [ ] Upload a post
+- [ ] Edit the post (Delete)
 - [ ] View the feed
---
+++ src/api/User/computed.js
@@ -0,0 +1,23 @@
+import { prisma } from '../../../generated/prisma-client';
+
+export default {
+  User: {
+    fullName: (parent) => {
+      return `${parent.firstName} ${parent.lastName}`;
+    },
+    isFollowing: (parent, __, { request }) => {
+      const { user } = request;
+      const { id: parentId } = parent;
+      try {
+        return prisma.$exists.user({ AND: [{ id: parentId }, { followers_some: { id: user.id } }] });
+      } catch {
+        return false;
+      }
+    },
+    isMyself: (parent, __, { request }) => {
+      const { user } = request;
+      const { id: parentId } = parent;
+      return user.id === parentId;
+    },
+  },
+};
